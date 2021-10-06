b82906370aba9080ce8bac3946693d222f10d620
david luis <davidluis2020@gmail.com>
2021-10-06 11:58:23 +0100

    scss

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,7 +3,18 @@ import "./topbar.scss"
 export default function Topbar() {
   return (
     <div className="topbar">
-      asdasda1
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="name"> David Luís</a>
+        </div>
+        <div className="right">
+          <div className="hamburguer">
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
