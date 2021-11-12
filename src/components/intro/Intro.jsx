0b2063a302068ab5c283fce015276c051f246124
david luis <davidluis2020@gmail.com>
2021-11-12 15:07:23 +0000

    intro work

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,25 @@
 import "./intro.scss"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookOpen, faBriefcase} from '@fortawesome/free-solid-svg-icons';
 
 export default function Intro() {
   return (
     <div className="intro" id="intro">
-      asdasda2
+      <div className="left">
+        <div className="wrapper">
+          <h2> Hello there I'm</h2>
+          <h1>David Luis</h1>
+          <h3>Web Developer</h3>
+        </div>
+        <a href="#curriculum">
+          <FontAwesomeIcon icon={faBookOpen} size="6x"></FontAwesomeIcon>
+        </a>
+      </div>
+      <div className="right">
+        <div className="imgContainer">
+          <img src="/Images/assests/david.png" alt=""/>
+        </div>
+      </div>
     </div>
   )
 }
