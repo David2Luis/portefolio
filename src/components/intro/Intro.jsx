60e0d63fc3532c338cc74e79add6d08aac2ee5f1
david luis <davidluis2020@gmail.com>
2021-11-15 11:27:54 +0000

    dynamic type ityped

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,15 +1,25 @@
 import "./intro.scss"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBookOpen, faBriefcase} from '@fortawesome/free-solid-svg-icons';
+import { init } from 'ityped';
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, { 
+      showCursor: false, 
+      strings: ['FullStack', 'OutSystems', 'Backend', 'Frontend' ] 
+    });
+  },[])
   return (
     <div className="intro" id="intro">
       <div className="left">
         <div className="wrapper">
           <h2> Hello there I'm</h2>
           <h1>David Luis</h1>
-          <h3>Web Developer</h3>
+          <h3><span ref={useRef}></span>Developer </h3>
         </div>
         <a href="#curriculum">
           <FontAwesomeIcon icon={faBookOpen} size="3x"></FontAwesomeIcon>
