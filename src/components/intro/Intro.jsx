7fcf616a9d73ab03060c1d02ebd935515ef76e08
david luis <davidluis2020@gmail.com>
2021-11-15 13:04:23 +0000

    problem solved thanks StackOverflow

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,31 +1,34 @@
 import "./intro.scss"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBookOpen,} from '@fortawesome/free-solid-svg-icons';
-import { init } from 'ityped';
+import { init } from "ityped";
 import { useEffect, useRef } from "react";
 
 export default function Intro() {
   const textRef = useRef();
 
   useEffect(() => {
-    init(textRef.current, { 
-      showCursor: false, 
-      strings: ["FullStack", "OutSystems", "Backend", "Frontend" ] 
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed:60,
+      strings: ["Web", "Backend", "Frontend", "OutSystems"],
     });
-  },[])
+  }, []);
 
   return (
     <div className="intro" id="intro">
       <div className="left">
-        <div className="wrapper">
-          <h2> Hello there I'm</h2>
-          <h1>David Luis</h1>
+      <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>David Luís</h1>
           <h3>
-            <span ref={useRef}></span> Developer
+            <span ref={textRef}></span> 
+            Developer
           </h3>
         </div>
         <a href="#curriculum">
-          <FontAwesomeIcon icon={faBookOpen} size="3x"></FontAwesomeIcon>
+          <img src="assets/down.png" alt="" />
         </a>
       </div>
       <div className="right">
@@ -34,5 +37,5 @@ export default function Intro() {
         </div>
       </div>
     </div>
-  )
+  );
 }
